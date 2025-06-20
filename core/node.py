f51f8d5b1b2493af08bfc0606f6a22f5af950177
Juawo <joaopedrojp2703@gmail.com>
2025-06-20 18:14:50 -0300

    Adicionando estrutura da classe Node Co-authored-by: Christian David  <ChrisDav05@users.noreply.github.com>

---
+++ core/node.py
@@ -0,0 +1,38 @@
+class Node:
+
+    # Construturo da classe
+    def __init__(self, state, parent=None, action=None, path_cost=0, depth=0):
+        self.state = state,
+        self.parent = parent,
+        self.action = action,
+        self.path_cost = path_cost,
+        self.depth = depth
+    
+    # Compara o menor custo de caminho entre dois Nós
+    def __lt__(self, other):
+        return self.path_cost < other.path_cost
+    
+    #   Função para expandir um nó, ou seja, ver quais filhos ele possui, salvando os resultados das ações possíveis nele 
+    def expand(self, problem):
+        children = []
+        for action in problem.actions(self.state):
+            new_state = problem.result(self.state, action)
+            cost = problem.path_cost(self.path_cost, self.state, action, new_state)
+            child = Node(
+                state=new_state,
+                parent=self,
+                action=action,
+                path_cost=cost,
+                depth=self.depth + 1
+            )
+            children.append(child)
+        return children
+
+    #   Função para encontrar qual o caminho de ações para aquele Nó/Estado 
+    def solution(self):
+        actions = []
+        node = self
+        while node.parent is not None:
+            actions.append(node.action)
+            node = node.parent
+        return list(reversed(actions))
